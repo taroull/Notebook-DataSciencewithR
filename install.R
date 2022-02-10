2f44a2cf2d1713ef5858d3674eb3057e84467a00
Carlos Pérez-González <cpgonzal@gmail.com>
2022-02-10 17:26:23 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-install.packages(c('dplyr','tidyr','reshape2','ggplot2','gridExtra','devtools'), dependencies=TRUE)
+install.packages(c('dplyr','tidyr','reshape2','ggplot2','gridExtra','devtools'), dependencies=TRUE,repos='http://cran.rstudio.com/')
